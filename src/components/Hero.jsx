113ff9f197f4496ac7ce500e2d39954c6c0e3489
patrick tunga-lergo <ptlergo@gmail.com>
2026-09-16 23:27:52 -0400

    create hero comp

---
+++ src/components/Hero.jsx
@@ -0,0 +1,68 @@
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/all';
+import gsap from 'gsap';
+ 
+const Hero = () => {
+    useGSAP(()=> {
+        // create the split texts
+        const heroSplit = new SplitText('.title', { type: ' words, chars' });
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+
+        // modify the chars of the hero text being split
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+        // animate the hero text
+        gsap.from(heroSplit.chars,{
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.06
+        })
+
+        // animate the paragraph text
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            y: 100,
+            duration:1.8,
+            ease: 'expo.out', 
+            stagger: 0.6,
+            delay: 1
+        })
+        
+        // animate the leafs 
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        }).to('.left-leaf', {y: 200},0).to('.right-leaf', {y: -200},0 )
+    }, [])
+
+    return (<><section id="hero" className="noisy">
+        <h1 className="title">title</h1>
+        <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
+        <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
+        <div className="body">
+            <div className="content">
+                <div className="space-y-5 hidden md:block">
+                    <p>cool crisp classic</p>
+                    <p className="subtitle">
+                        sip the spirit of<br /> summer
+                    </p>
+                </div>
+                <div className="view-cocktails">
+                    <p className="subtitle">
+                        Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable.
+                    </p>
+                    <a href="#cocktails"  >
+                        View Cocktails
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section></>)
+}
+
+export default Hero;
